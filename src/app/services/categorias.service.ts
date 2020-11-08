0064b00eada66f161f499f8834ff2c88692f65f3
BermeoA14 <47487527+Joanber@users.noreply.github.com>
2020-11-08 13:09:35 -0500

    endpoint de categorias filtradas por nombre

--- src/app/services/categorias.service.ts
+++ src/app/services/categorias.service.ts
@@ -1,33 +1,35 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { BASE_ENDPOINT } from '../DB_CONFIG/bdConig';
-import { Categoria } from '../models/categoria';
-import { Observable, throwError } from 'rxjs';
-import { map, catchError, tap } from 'rxjs/operators';
-import Swal from 'sweetalert2';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { BASE_ENDPOINT } from "../DB_CONFIG/bdConig";
+import { Categoria } from "../models/categoria";
+import { Observable, throwError } from "rxjs";
+import { map, catchError, tap } from "rxjs/operators";
+import Swal from "sweetalert2";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class CategoriasService {
-  constructor( private http: HttpClient ) { }
-  protected baseEndpoint = BASE_ENDPOINT + '/categorias';
+  constructor(private http: HttpClient) {}
+  protected baseEndpoint = BASE_ENDPOINT + "/categorias";
   getCategorias(): Observable<Categoria[]> {
     return this.http.get<Categoria[]>(`${this.baseEndpoint}/`);
   }
 
-  getCategoriasFiltro(termino: string): Observable<Categoria[]>{
-    return this.http.get<Categoria[]>(`${this.baseEndpoint}/filtrar/${termino}`);
+  getCategoriasFiltro(termino: string): Observable<Categoria[]> {
+    return this.http.get<Categoria[]>(
+      `${this.baseEndpoint}/filtrar/${termino}`
+    );
   }
 
   crearCategoria(categoria: Categoria): Observable<Categoria> {
     return this.http.post<Categoria>(`${this.baseEndpoint}/`, categoria).pipe(
-      map((response:any)=> response.categoria as Categoria),
-      catchError(e => {
-        if (e.status ==400) {
+      map((response: any) => response.categoria as Categoria),
+      catchError((e) => {
+        if (e.status == 400) {
           return throwError(e);
         }
-        Swal.fire('Error al crear la categoría: ',e.error.mensaje,'error');
+        Swal.fire("Error al crear la categoría: ", e.error.mensaje, "error");
         return throwError(e);
       })
     );
@@ -51,22 +53,20 @@ export class CategoriasService {
       );
   }
 
-  getCategoriasPage(page: string): Observable<any>{
-    return this.http.get(this.baseEndpoint+'/page/'+page).pipe(
-      tap((response:any) => {
-        (response.content  as Categoria[]).forEach(categoria => {
+  getCategoriasPage(page: string): Observable<any> {
+    return this.http.get(this.baseEndpoint + "/page/" + page).pipe(
+      tap((response: any) => {
+        (response.content as Categoria[]).forEach((categoria) => {
           console.log(categoria.nombre);
-        })
+        });
       }),
-      map((response:any) => {
-        (response.content as Categoria[]).map(producto => {
-  
+      map((response: any) => {
+        (response.content as Categoria[]).map((producto) => {
           return producto;
-        })
-        return response
+        });
+        return response;
       })
-  
-    )
+    );
   }
 
   editarConFoto(categoria: Categoria, archivo: File): Observable<Categoria> {
@@ -74,7 +74,10 @@ export class CategoriasService {
     formData.append("archivo", archivo);
     formData.append("nombre", categoria.nombre);
     return this.http
-      .put<Categoria>(`${this.baseEndpoint}` + `/editar-con-foto/${categoria.id}`, formData)
+      .put<Categoria>(
+        `${this.baseEndpoint}` + `/editar-con-foto/${categoria.id}`,
+        formData
+      )
       .pipe(
         map((response: any) => response.producto as Categoria),
         catchError((e) => {
@@ -88,28 +91,36 @@ export class CategoriasService {
   }
 
   editarCategoria(categoria: Categoria): Observable<Categoria> {
-    return this.http.put<Categoria>(`${this.baseEndpoint}/${categoria.id}`, categoria).pipe(
-      map((response:any)=> response.categoria as Categoria),
-      catchError(e => {
-        if (e.status ==400) {
+    return this.http
+      .put<Categoria>(`${this.baseEndpoint}/${categoria.id}`, categoria)
+      .pipe(
+        map((response: any) => response.categoria as Categoria),
+        catchError((e) => {
+          if (e.status == 400) {
+            return throwError(e);
+          }
+          Swal.fire("Error al editar la categoría: ", e.error.mensaje, "error");
           return throwError(e);
-        }
-        Swal.fire('Error al editar la categoría: ',e.error.mensaje,'error');
-        return throwError(e);
-      })
-    );
+        })
+      );
   }
 
-  eliminar(id:number): Observable<Categoria> {
+  eliminar(id: number): Observable<Categoria> {
     return this.http.delete<Categoria>(`${this.baseEndpoint}/${id}`).pipe(
-      catchError(e =>{
-        Swal.fire('Error al eliminar la categoría ',e.error.mensaje,'error');
+      catchError((e) => {
+        Swal.fire("Error al eliminar la categoría ", e.error.mensaje, "error");
         return throwError(e);
       })
     );
   }
 
-  getCategoria(id:number): Observable<Categoria> {
+  getCategoria(id: number): Observable<Categoria> {
     return this.http.get<Categoria>(`${this.baseEndpoint}/${id}`);
+  }
+
+  getCategoriasFiltradas(termino: string): Observable<Categoria[]> {
+    return this.http.get<Categoria[]>(
+      `${this.baseEndpoint}/filtrar/${termino}`
+    );
   }
 }
